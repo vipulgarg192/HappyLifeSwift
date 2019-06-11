61fb42b7eddaa60d4e9a95872631bd97ea6e472c
VIPUL <VIPULGARG192@GMAIL.COM>
2019-06-11 18:21:03 -0400

    Initial Commit

---
+++ HappyLifeSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HappyLifeSwift
+//
+//  Created by MacStudent on 2019-06-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
